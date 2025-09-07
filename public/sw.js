631e16745ea4f56f52bd97b028ed531a180378d4
Paden Clayton <me@padenclayton.com>
2025-09-06 19:40:30 -0500

    Added fixes for PWA issues in lighthouse report.

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,35 @@
-const CACHE_NAME = 'outfit-composer-v1';
+const CACHE_NAME = 'outfit-composer-v2';
 const urlsToCache = [
   '/',
-  '/static/js/bundle.js',
-  '/static/css/main.css',
-  '/manifest.json'
+  '/manifest.json',
+  '/what-to-wear-logo.svg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
   );
 });
 
@@ -17,11 +37,28 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        if (response) {
-          return response;
+        // Return cached version or fetch from network
+        return response || fetch(event.request).then((fetchResponse) => {
+          // Don't cache non-successful responses
+          if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+            return fetchResponse;
+          }
+
+          // Clone the response
+          const responseToCache = fetchResponse.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return fetchResponse;
+        });
+      }).catch(() => {
+        // If both cache and network fail, return offline page for navigation requests
+        if (event.request.destination === 'document') {
+          return caches.match('/');
         }
-        return fetch(event.request);
-      }
-    )
+      })
   );
 });
